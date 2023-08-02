4fc0934df7761819241dc8406a58f2b608766521
0xdjrdj <august.ktf@gmail.com>
2023-08-02 15:59:04 +0900

    Modified: - svelte.config.js: runtime: 'edge'

--- svelte.config.js
+++ svelte.config.js
@@ -14,14 +14,12 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	runtime: 'edge',
-
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'nodejs18.x'
+			runtime: 'edge'
 		}),
 	},
 };
