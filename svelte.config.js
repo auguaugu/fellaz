79493f7206e37e2b043bbbcfc6f383ea9ddf52f8
0xdjrdj <august.ktf@gmail.com>
2023-08-02 16:42:59 +0900

    Modified: svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
-		adapter: adapter(),
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter ({
+			runtime: 'edge'
+		}),
 	},
 };
 
@@ -14,12 +19,4 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			runtime: 'edge'
-		}),
-	},
 };
